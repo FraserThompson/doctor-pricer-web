c503ab7ccd7dbd3cce102f04d2ee64d33eae4b9f
Fraser Thompson <fraserthompson0@gmail.com>
2018-08-11 15:01:48 +1200

    Build

--- app/index.js
+++
@@ -1,9 +0,0 @@
-require('./styles/bootstrap.css');
-require('./styles/main.css');
-require('./favicon.ico')
-require('./robots.txt')
-
-require("leaflet_css");
-require("leaflet_marker");
-require("leaflet_marker_2x");
-require("leaflet_marker_shadow");
