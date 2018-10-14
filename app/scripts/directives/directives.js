0a85a6394e1bc026e9d79e97f6edd8f1b74f16cb
Fraser Thompson <fraserthompson0@gmail.com>
2018-10-14 16:05:20 +1300

    try again

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -64,11 +64,11 @@ angular.module('doctorpricerWebApp')
 		return {
 			replace: 'true',
 			template: '<div class="stupid-ad {{class}}"> \
-						<ins class="adsbygoogle" \
+						<ins class="adsbygoogle stupid-adsbygoogle" \
 						style="display:block" \
 						data-ad-client="ca-pub-2527917281752489" \
 						data-ad-slot="1395256542" \
-						data-ad-format="auto,horizontal" \
+						data-ad-format="auto" \
 						data-full-width-responsive="true" \
 						</ins> \
 						</div>',
